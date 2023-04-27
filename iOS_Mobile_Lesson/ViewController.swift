f7cc349f959d207f5c0cd3dbb1cc2c8008df4021
Mehmet Emin Ak <mak44162@gmail.com>
2023-04-27 13:09:58 +0300

    Initial Commit

---
+++ iOS_Mobile_Lesson/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iOS_Mobile_Lesson
+//
+//  Created by Macbook Air on 27.04.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
